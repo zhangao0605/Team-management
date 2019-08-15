25e5f6de6767ff7ffebe8a7649c7ac577c557e11
zhangao <zhangao@digvita.com>
2019-08-15 10:45:01 +0800

    1.0.2

--- src/utils/js/request.js
+++ src/utils/js/request.js
@@ -26,14 +26,6 @@ service.interceptors.request.use(
     Promise.reject(error)
   }
 )
-
-// response 拦截器
-/**
- * 下面的注释为通过在response里，自定义code来标示请求状态
- * 当code返回如下情况则说明权限有问题，登出并返回到登录页
- * 如想通过 XMLHttpRequest 来状态码标识 逻辑可写在下面error中
- * 以下代码均为样例，请结合自生需求加以修改，若不需要，则可删除
- */
 service.interceptors.response.use(
   response => {
     const res = response.data
